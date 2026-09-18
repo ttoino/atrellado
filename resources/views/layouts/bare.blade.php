34fb0d729342c5cd361912e719cc2fe4bf08bfb7
João Pereira <joaop@cloudflare.com>
2026-09-18 23:56:21 +0100

    Drop unused vendor packages

--- resources/views/layouts/bare.blade.php
+++ resources/views/layouts/bare.blade.php
@@ -44,7 +44,7 @@
 
     <div class="toast-container position-fixed bottom-0 end-0 p-3">
     </div>
-    @include('cookie-consent::index')
+    @include('partials.cookie-consent')
 </body>
 
 </html>
