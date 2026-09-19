4e84d212949f0703a28638bce12b12fb409ef82f
João Pereira <joaop@cloudflare.com>
2026-09-19 16:58:18 +0100

    style the listener union type hints

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,11 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\AuthServiceProvider;
-use App\Providers\EventServiceProvider;
 use App\Providers\TelescopeServiceProvider;
 
 return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\AuthServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+    AppServiceProvider::class,
+    AuthServiceProvider::class,
+    TelescopeServiceProvider::class,
 ];
