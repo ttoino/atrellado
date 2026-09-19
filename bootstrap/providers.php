2755615be7492b567f3ac2116c4a7685d53386e6
João Pereira <joaop@cloudflare.com>
2026-09-19 16:31:30 +0100

    add telescope for local debugging

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Providers\AppServiceProvider;
-use App\Providers\AuthServiceProvider;
-use App\Providers\EventServiceProvider;
-
 return [
-    AppServiceProvider::class,
-    AuthServiceProvider::class,
-    EventServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
